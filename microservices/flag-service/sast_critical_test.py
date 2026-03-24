fe1b6da60b50b086133571e1c4a353462425d8c3
dsrdantas <dantas2dantas@msn.com>
2026-03-24 14:14:41 -0300

    TC3

--- microservices/flag-service/sast_critical_test.py
+++ microservices/flag-service/sast_critical_test.py
@@ -4,6 +4,7 @@ Remover apos o teste.
 """
 
 import psycopg2
+import subprocess
 
 
 def vulnerable_sql(user_input: str, conn: psycopg2.extensions.connection):
@@ -12,3 +13,9 @@ def vulnerable_sql(user_input: str, conn: psycopg2.extensions.connection):
     # Vulnerabilidade proposital para acionar SAST (SQL injection)
     cur.execute("SELECT * FROM flags WHERE name = '%s'" % user_input)
     cur.close()
+
+
+def vulnerable_command(user_input: str):
+    """Intencionalmente inseguro: command injection (SAST)"""
+    # Vulnerabilidade proposital para acionar SAST (command injection)
+    subprocess.run(user_input, shell=True, check=False)
